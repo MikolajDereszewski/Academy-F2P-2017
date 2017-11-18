f4c4c07c0d2292fe12198df5b9bfb5993cc2af78
Mikołaj Dereszewski <mikolaj.dereszewski@gmail.com>
2017-11-18 13:35:28 +0100

    Cheeky Breeky ImageEffects for prezentation and obstacles images

--- F2P_Project/Assets/Scripts/Platform.cs
+++ F2P_Project/Assets/Scripts/Platform.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using GameClasses;
 
 public class Platform : MonoBehaviour {
@@ -6,7 +7,7 @@ public class Platform : MonoBehaviour {
     public PlatformProperties Properties { get { return _properties; } }
     
     [SerializeField]
-    private ScrollingObject _obstaclePrefab = null;
+    private List<ScrollingObject> _obstaclePrefabs = null;
     [SerializeField]
     private SpriteRenderer _renderer1, _renderer2;
 
@@ -31,9 +32,10 @@ public class Platform : MonoBehaviour {
         int obstaclesCount = Random.Range(1, (int)(_properties.Length/2f));
         for(int i = 0; i < obstaclesCount; i++)
         {
-            float randomizer = (_properties.Length * 0.5f - (_obstaclePrefab.transform.localScale.x * 0.5f));
-            Vector3 spawnPosition = new Vector3(Random.Range(-randomizer, randomizer) + transform.position.x, _obstaclePrefab.transform.position.y, _obstaclePrefab.transform.position.z);
-            Instantiate(_obstaclePrefab, spawnPosition, Quaternion.identity);
+            int index = Random.Range(0, _obstaclePrefabs.Count);
+            float randomizer = (_properties.Length * 0.5f - (_obstaclePrefabs[index].transform.localScale.x * 0.5f));
+            Vector3 spawnPosition = new Vector3(Random.Range(-randomizer, randomizer) + transform.position.x, transform.position.y, _obstaclePrefabs[index].transform.position.z);
+            Instantiate(_obstaclePrefabs[index], spawnPosition, Quaternion.identity);
         }
     }
 
