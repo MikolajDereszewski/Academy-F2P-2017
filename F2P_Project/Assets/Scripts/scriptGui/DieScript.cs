9b4ae25f75f75759a9d400470497c8de5ccaed5a
Sebastian <sebek2088s4@tlen.pl>
2017-11-18 09:05:10 +0100

    Complete gui

---
+++ F2P_Project/Assets/Scripts/scriptGui/DieScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DieScript : MonoBehaviour {
+    const string PREF_NAME = MainMenu.NAME_PREF;
+
+    public Text textScore;
+    public Text textBestScore;
+	
+	void Start () {
+        if (PlayerPrefs.HasKey(PREF_NAME))
+            textBestScore.text = "Najlepszy wynik: " + PlayerPrefs.GetInt(PREF_NAME);
+        else
+            textBestScore.text = "Najlepszy wynik: " + 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- F2P_Project/Assets/Scripts/scriptGui/PlayMenu.cs
+++ F2P_Project/Assets/Scripts/scriptGui/PlayMenu.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GUIOption : MonoBehaviour {
-
-	public void OpenScene(string name)
+public class PlayMenu : MonoBehaviour {
+    public void OpenScene(string name)
     {
         SceneManager.LoadScene(name);
     }
